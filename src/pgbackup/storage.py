867ed8c2e69a48020b3237f19e9a57356a1b8a68
Raminder Singh <raminderis@live.com>
2019-02-03 20:07:59 +0000

    delete master

--- src/pgbackup/storage.py
+++
@@ -1,7 +0,0 @@
-def local(infile, outfile):
-    outfile.write(infile.read())
-    outfile.close()
-    infile.close()
-
-def s3(client, infile, bucket, filename):
-    client.upload_fileobj(infile,bucket,filename)
